d212d35b07e54961407f927def761f25527db7bb
Victor Razo <victor-razo@outlook.com>
2023-08-22 23:17:14 -0600

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import {useState, useEffect} from "react";
+import Header from "./components/Header.jsx";
+import Formulario from "./components/Formulario.jsx";
+import ListadoPacientes from "./components/ListadoPacientes.jsx";
+function App() {
+
+    const [pacientes,setPacientes] = useState([])
+    const [paciente, setPaciente] = useState({})
+
+
+
+    useEffect(()=>{
+        const obtenerLS = ()=>{
+            const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+            setPacientes(pacientesLS);
+            console.log(pacientesLS)
+        }
+        obtenerLS();
+    },[])
+
+    useEffect(()=>{
+        localStorage.setItem("pacientes",JSON.stringify(pacientes))
+    },[pacientes])
+
+
+
+
+
+    const eliminarPaciente = id => {
+        const pacientesActualizados = pacientes.filter(paciente => paciente.id !== id)
+        setPacientes(pacientesActualizados);
+    }
+
+  return (
+    <div className="container mx-auto mt-20">
+        <Header
+
+        />
+        <div className="mt-12 md:flex">
+            <Formulario
+                pacientes = {pacientes}
+                setPacientes = {setPacientes}
+                paciente = {paciente}
+                setPaciente = {setPaciente}
+            />
+            <ListadoPacientes
+                pacientes = {pacientes}
+                setPaciente = {setPaciente}
+                eliminarPaciente = {eliminarPaciente}
+            />
+        </div>
+
+    </div>
+  )
+}
+
+export default App
